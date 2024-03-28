31bb8c7aeb2a8769de495f8a75567b3074910641
Olivier <olivier.astre@outlook.fr>
2024-03-28 11:41:04 +0100

    todoetails

--- front/src/components/TodoDetails.tsx
+++ front/src/components/TodoDetails.tsx
@@ -1,10 +1,24 @@
 import { Todo } from '../models/Todo';
 
 const TodoDetails = ({ todo }: { todo: Todo }) => {
+  let checkClasses = `far fa-3x fa${todo.isDone ? '-check' : ''}-circle`;
   return (
-    <div className='card'>
+    <div className='card mt-2'>
       <div className='card-body'>
-        <h5 className='card-title'>{todo.title}</h5>
+        <div className='d-flex'>
+          <i className={checkClasses}></i>
+          <div className='ms-3'>
+            <h5 className='card-title'>{todo.title}</h5>
+            <span className='badge text-bg-dark'>
+              {todo.dueDate.toLocaleDateString()}
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className='card-footer text-end'>
+        <button className='btn btn-link text-danger'>
+          <i className='fa fa-trash'></i>
+        </button>
       </div>
     </div>
   );
